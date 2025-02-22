77f0b38a2b58b08b565946f0f54e4b2e4773610e
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2025-02-22 11:46:37 +0200

    changes path icons.svg assets index.html

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import { fileURLToPath } from "url";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  base: '/scss-project/',
   plugins: [
     ViteImageOptimizer({
       png: {
